31e1e6c4ef9b3d882bfad8bc800a4bd626dec852
jayakesavanmuthazhagan <kesavan.gm@gmail.com>
2024-07-28 17:20:57 -0600

    Add junit

--- src/test/java/com/adobe/convertor/controller/NumberToRomanControllerTest.java
+++ src/test/java/com/adobe/convertor/controller/NumberToRomanControllerTest.java
@@ -7,7 +7,6 @@ package com.adobe.convertor.controller;
  */
 
 
-
 import com.adobe.convertor.bean.ConversionResponse;
 import com.adobe.convertor.bean.ConversionResult;
 import com.adobe.convertor.exception.InvalidInputException;
@@ -15,24 +14,17 @@ import com.adobe.convertor.service.NumberToRomanService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -93,7 +85,7 @@ public class NumberToRomanControllerTest {
                         .param("query", "invalid")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof InvalidInputException));
+                .andExpect(result -> assertInstanceOf(InvalidInputException.class, result.getResolvedException()));
 
         verify(numberToRomanService, never()).convertToRomanNumeral(anyInt());
     }
@@ -107,7 +99,7 @@ public class NumberToRomanControllerTest {
                         .param("max", "3")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof InvalidInputException));
+                .andExpect(result -> assertInstanceOf(InvalidInputException.class, result.getResolvedException()));
 
         verify(numberToRomanService, times(1)).convertRangeToRoman(5, 3);
     }
