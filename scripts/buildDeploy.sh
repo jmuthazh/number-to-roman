d1518dd976c3dc763b3f752043e00ee64e7e0dc9
jayakesavanmuthazhagan <kesavan.gm@gmail.com>
2024-07-30 23:55:22 -0600

    Clean up builDeploy.sh

--- scripts/buildDeploy.sh
+++ scripts/buildDeploy.sh
@@ -6,8 +6,6 @@ set -e
 # Print each command before executing it (useful for debugging)
 set -x
 
-# Run Maven clean install with the latest updates
-#mvn clean install -U
 
 # Start Docker Compose services with build
 docker-compose -f ../docker/docker-compose.yml up -d --build
